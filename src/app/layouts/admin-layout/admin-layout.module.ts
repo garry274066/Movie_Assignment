a37897a5b41ca26bded800ed10ea59e58253b47d
garry274066 <garry274066@gmail.com>
2021-03-14 23:51:58 -0600

    Commiting code changes

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -7,10 +7,7 @@ import { ClipboardModule } from 'ngx-clipboard';
 
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
-import { IconsComponent } from '../../pages/icons/icons.component';
-import { MapsComponent } from '../../pages/maps/maps.component';
-import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
-import { TablesComponent } from '../../pages/tables/tables.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {TableModule} from 'primeng/table';
 import { AddMoviesComponent } from '../../pages/add-movies/add-movies.component'
@@ -29,10 +26,7 @@ import { MovieDetailsComponent } from '../../pages/movie-details/movie-details.c
   ],
   declarations: [
     DashboardComponent,
-    UserProfileComponent,
-    TablesComponent,
-    IconsComponent,
-    MapsComponent,
+   
     AddMoviesComponent,
     MovieDetailsComponent 
     
--- src/app/layouts/auth-layout/auth-layout.module.ts
+++ src/app/layouts/auth-layout/auth-layout.module.ts
@@ -6,7 +6,7 @@ import { AuthLayoutRoutes } from './auth-layout.routing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { LoginComponent } from '../../pages/login/login.component';
-import { RegisterComponent } from '../../pages/register/register.component';
+
 
 @NgModule({
   imports: [
@@ -17,7 +17,7 @@ import { RegisterComponent } from '../../pages/register/register.component';
   ],
   declarations: [
     LoginComponent,
-    RegisterComponent
+ 
   ]
 })
 export class AuthLayoutModule { }
